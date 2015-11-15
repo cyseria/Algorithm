bcc6791269602a15ba53de91ea4410441d6ef370
cyseria <xcyseria@gmail.com>
2015-11-15 22:14:40 +0800

    分治法求众数bug fixed Signed-off-by: cyseria <xcyseria@gmail.com>

--- 源码/2-6.cpp
+++ 源码/2-6.cpp
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 int MAXCOUNT = 0, //众数重数
@@ -38,15 +39,29 @@ void getMode(int a[], int n)
         getMode(a+r,n-r);
     }
 }
+
+//排序
+void insertSort(int arr[], int n){
+	for(int i = 1;i < n;i++){
+		int temp = arr[i];
+		int j = i - 1;
+		while(temp < arr[j]){
+			arr[j+1] = arr[j];
+			j--;
+			if(j == -1)	break;
+		}
+		arr[j+1] = temp;
+	}
+}
 int main()
 {
-
     int n;
     scanf("%d",&n);
     int *a = new int[n];
     for (int i = 0; i < n; i++) {
         scanf("%d",&a[i]);
     }
+    insertSort(a,n);
     getMode(a,n);
     printf("%d %d",NUM,MAXCOUNT);
     return 0;
